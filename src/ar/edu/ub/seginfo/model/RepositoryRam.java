7fead438a113e66f14eef990efc07408079aad89
wduartes <walterduartes@gmail.com>
2018-08-17 15:32:27 -0300

    Se agrega comportamiento al repositorio en ram de los bloques

--- src/ar/edu/ub/seginfo/model/RepositoryRam.java
+++ src/ar/edu/ub/seginfo/model/RepositoryRam.java
@@ -1,10 +1,30 @@
 package ar.edu.ub.seginfo.model;
 
-public class RepositoryRam implements IRepository<Block> {
+import java.util.LinkedList;
+import java.util.List;
 
+public class RepositoryRam implements IRepository<Block> {
+	private List<Block> blocks;
+	
+	public RepositoryRam()
+	{
+		this.setBlocks( new LinkedList<Block>() );
+	}
+	
 	@Override
 	public boolean add(Block dato) {
+		
+		this.getBlocks().add( dato );
+		
 		return false;
+	}
+
+	public List<Block> getBlocks() {
+		return blocks;
+	}
+
+	private void setBlocks(List<Block> blocks) {
+		this.blocks = blocks;
 	}
 
 }
