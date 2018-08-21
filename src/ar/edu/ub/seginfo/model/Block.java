f63d8afc2ae2b3998845bb3cf28dbc857ab8f396
WalterDuartesUB <walter.duartes@comunidad.ub.edu.ar>
2018-08-21 11:42:10 -0300

    Se agregan los campos para la implementacion del bloque

--- src/ar/edu/ub/seginfo/model/Block.java
+++ src/ar/edu/ub/seginfo/model/Block.java
@@ -1,12 +1,60 @@
 package ar.edu.ub.seginfo.model;
 
+import ar.edu.ub.seginfo.cipher.Cipher;
+
 public class Block {
-	private String timestamping;
+	private int index;
+	private long timestamping;
+	private String hash;
+	private String previousHash;	
 	private String data;
 	
-	public Block(String timestamping, String data) {
-		this.setTimestamping(timestamping);
-		this.setData(data);
+	private Cipher cipher;
+	
+	public Block(int index, String previousHash, String data, Cipher cipher){		
+		this.setIndex(index);
+		this.setPreviousHash(previousHash);
+		this.setData(data);		
+		this.setTimestamping( System.currentTimeMillis() );
+		this.setCipher(cipher);
+		
+		this.calculateHash();
+	}
+
+	private void calculateHash() {		
+		this.setHash( this.getCipher().generateHash( this.getHashData() ) );		
+	}
+
+	public int getIndex() {
+		return index;
+	}
+
+	private void setIndex(int index) {
+		this.index = index;
+	}
+
+	public long getTimestamping() {
+		return timestamping;
+	}
+
+	private void setTimestamping(long timestamping) {
+		this.timestamping = timestamping;
+	}
+
+	public String getHash() {
+		return hash;
+	}
+
+	private void setHash(String hash) {
+		this.hash = hash;
+	}
+
+	public String getPreviousHash() {
+		return previousHash;
+	}
+
+	private void setPreviousHash(String previousHash) {
+		this.previousHash = previousHash;
 	}
 
 	public String getData() {
@@ -17,13 +65,16 @@ public class Block {
 		this.data = data;
 	}
 
-	public String getTimestamping() {
-		return timestamping;
+	public Cipher getCipher() {
+		return cipher;
 	}
 
-	private void setTimestamping(String timestamping) {
-		this.timestamping = timestamping;
+	private void setCipher(Cipher cipher) {
+		this.cipher = cipher;
 	}
-	
+		
+	private String getHashData() {
+		return String.format("%d%s%d%s", this.getIndex(), this.getData(), this.getTimestamping(), this.getPreviousHash() );
+	}
 	
 }
