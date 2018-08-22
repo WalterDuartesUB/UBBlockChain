4f0932fe64cfb5771aab68fb7c7db34f35057651
WalterDuartesUB <walter.duartes@comunidad.ub.edu.ar>
2018-08-22 09:58:19 -0300

    Aparece la implementacion para el repositorio de los Blocks

--- src/ar/edu/ub/seginfo/model/IRepositoryBlockChain.java
+++ src/ar/edu/ub/seginfo/model/IRepositoryBlockChain.java
@@ -3,6 +3,7 @@ package ar.edu.ub.seginfo.model;
 import java.util.Collection;
 
 public interface IRepositoryBlockChain {
+	boolean isEmpty();
 	IBlock	getLastBlock();
 	void	add( IBlock block );
 	void 	getAll(Collection<IBlock> collection);
---
+++ src/ar/edu/ub/seginfo/model/RepositoryBlockChainRam.java
@@ -0,0 +1,43 @@
+package ar.edu.ub.seginfo.model;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+public class RepositoryBlockChainRam implements IRepositoryBlockChain {
+	
+	private List< IBlock > blocks;
+
+	public RepositoryBlockChainRam() {
+		this.setBlocks( new LinkedList<IBlock>() );
+	}
+	
+	@Override
+	public IBlock getLastBlock() {
+		return this.getBlocks().get( this.getBlocks().size() - 1);
+	}
+
+	@Override
+	public void add(IBlock block) {
+		this.getBlocks().add( block );
+	}
+
+	@Override
+	public void getAll(Collection<IBlock> collection) {
+		collection.addAll( this.getBlocks() );
+	}
+
+	private List< IBlock > getBlocks() {
+		return blocks;
+	}
+
+	private void setBlocks(List< IBlock > blocks) {
+		this.blocks = blocks;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return this.getBlocks().isEmpty();
+	}
+
+}
