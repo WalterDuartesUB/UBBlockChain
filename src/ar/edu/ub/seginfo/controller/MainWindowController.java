8900739cfba590476698ef1abd2b491a9cbefa0a
WalterDuartesUB <walter.duartes@comunidad.ub.edu.ar>
2018-08-22 10:00:55 -0300

    Se agrega en el controlador de la ventana principal la inicializacion de la blockchain

--- src/ar/edu/ub/seginfo/controller/MainWindowController.java
+++ src/ar/edu/ub/seginfo/controller/MainWindowController.java
@@ -1,12 +1,15 @@
 package ar.edu.ub.seginfo.controller;
 
+import ar.edu.ub.seginfo.model.BlockChain;
+import ar.edu.ub.seginfo.model.RepositoryBlockChainRam;
 import ar.edu.ub.seginfo.view.MainWindowView;
 
 public class MainWindowController {
 	private MainWindowView view;
+	private BlockChain blockChain;
 	
 	public MainWindowController(){
-		
+		this.setBlockChain( new BlockChain( new RepositoryBlockChainRam() ) );
 	}
 
 	public MainWindowView getView() {
@@ -19,6 +22,14 @@ public class MainWindowController {
 
 	public void uploadFile( String filePath ) {
 		
+	}
+
+	private BlockChain getBlockChain() {
+		return blockChain;
+	}
+
+	private void setBlockChain(BlockChain blockChain) {
+		this.blockChain = blockChain;
 	}
 
 
