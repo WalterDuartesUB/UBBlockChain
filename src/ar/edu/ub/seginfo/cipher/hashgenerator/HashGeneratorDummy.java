c4bd1766dc8c1c768f23afef3f8c541f7c780936
WalterDuartesUB <37937023+WalterDuartesUB@users.noreply.github.com>
2018-09-11 21:54:54 -0300

    Restauro los cipher dummy

--- src/ar/edu/ub/seginfo/cipher/hashgenerator/HashGeneratorDummy.java
+++ src/ar/edu/ub/seginfo/cipher/hashgenerator/HashGeneratorDummy.java
@@ -1,31 +1,19 @@
 package ar.edu.ub.seginfo.cipher.hashgenerator;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
-import javax.xml.bind.DatatypeConverter;
+import java.nio.charset.StandardCharsets;
 
 public class HashGeneratorDummy implements IHashGenerator {
 
 	@Override
 	public String generateHash(String data) {
-		return this.generateHash( data.getBytes() );
+		return data;
 	}
 
 	@Override
 	public String generateHash(byte[] data) {
-		try {
-			MessageDigest md = MessageDigest.getInstance("MD5");
-		    
-			md.update(data);	    
-		    
-		    return DatatypeConverter.printHexBinary( md.digest() ).toUpperCase();
-		    
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		}
-		
-		return null;
+		return new String(data, StandardCharsets.UTF_8);
 	}
+
+
 
 }
