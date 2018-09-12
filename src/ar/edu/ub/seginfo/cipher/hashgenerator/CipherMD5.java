e960826fa7ee5d5029127f54446ee5c27d44df5f
WalterDuartesUB <37937023+WalterDuartesUB@users.noreply.github.com>
2018-09-11 21:51:39 -0300

    Se implementa el CipherMD5

--- src/ar/edu/ub/seginfo/cipher/hashgenerator/CipherMD5.java
+++ src/ar/edu/ub/seginfo/cipher/hashgenerator/CipherMD5.java
@@ -1,16 +1,30 @@
 package ar.edu.ub.seginfo.cipher.hashgenerator;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.xml.bind.DatatypeConverter;
+
 public class CipherMD5 implements IHashGenerator {
 
 	@Override
 	public String generateHash(String data) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.generateHash( data.getBytes() );
 	}
 
 	@Override
 	public String generateHash(byte[] data) {
-		// TODO Auto-generated method stub
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+		    
+			md.update(data);	    
+		    
+		    return DatatypeConverter.printHexBinary( md.digest() ).toUpperCase();
+		    
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		
 		return null;
 	}
 
