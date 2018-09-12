4110379787fb835198c74bfc9ba66fd7df1d7fe1
WalterDuartesUB <37937023+WalterDuartesUB@users.noreply.github.com>
2018-09-11 21:49:37 -0300

    Aparecen las implementaciones de AES y MD5

---
+++ src/ar/edu/ub/seginfo/cipher/bidirectionalcipher/CipherAES.java
@@ -0,0 +1,17 @@
+package ar.edu.ub.seginfo.cipher.bidirectionalcipher;
+
+public class CipherAES implements IBidirectionalCipher {
+
+	@Override
+	public String encrypt(String data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String decrypt(String hash) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/ar/edu/ub/seginfo/cipher/hashgenerator/CipherMD5.java
@@ -0,0 +1,17 @@
+package ar.edu.ub.seginfo.cipher.hashgenerator;
+
+public class CipherMD5 implements IHashGenerator {
+
+	@Override
+	public String generateHash(String data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateHash(byte[] data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
