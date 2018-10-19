3ed76abe5038773deed8701ef460b68240d13dcd
wduartes <walterduartes@gmail.com>
2018-10-19 11:29:13 -0300

    Se implementan los metodos para levantar la configuracion

--- src/ar/edu/ub/seginfo/util/Configuracion.java
+++ src/ar/edu/ub/seginfo/util/Configuracion.java
@@ -1,5 +1,63 @@
 package ar.edu.ub.seginfo.util;
 
-public class Configuracion {
+import java.io.Closeable;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
+public class Configuracion  implements Closeable{
+	
+	private Properties properties;
+	private FileInputStream fis;
+	
+	public Configuracion(String fileName) {
+		
+		this.setProperties(new Properties());
+		
+		try {
+			
+			this.setFis(new FileInputStream(fileName));
+			
+			getProperties().load(this.getFis());
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}			
+	}
+	
+	public String getConfiguracion(String propiedad) {
+		
+		return this.getProperties().getProperty(propiedad);
+		
+	}
+
+	public int getConfiguracionAsInt(String propiedad) {		
+		return Integer.parseInt( this.getProperties().getProperty(propiedad) );		
+	}
+
+	private Properties getProperties() {
+		return properties;
+	}
+
+	private void setProperties(Properties properties) {
+		this.properties = properties;
+	}
+
+	private FileInputStream getFis() {
+		return fis;
+	}
+
+	private void setFis(FileInputStream fis) {
+		this.fis = fis;
+	}
+
+	@Override
+	public void close() throws IOException {		
+		this.getFis().close();		
+	}
+	
+	
 }
