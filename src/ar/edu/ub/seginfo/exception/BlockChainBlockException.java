4b8d59251970177c878d897b5cd08c76289babe6
WalterDuartesUB <37937023+WalterDuartesUB@users.noreply.github.com>
2018-10-31 22:16:36 -0300

    Se agregan excepciones para validar la base de datos de bloques repetidos o invalidos

---
+++ src/ar/edu/ub/seginfo/exception/BlockChainBlockException.java
@@ -0,0 +1,13 @@
+package ar.edu.ub.seginfo.exception;
+
+public class BlockChainBlockException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public BlockChainBlockException(String message) {
+		super( message );
+	}
+}
--- src/ar/edu/ub/seginfo/model/IBlockChain.java
+++ src/ar/edu/ub/seginfo/model/IBlockChain.java
@@ -3,8 +3,9 @@ package ar.edu.ub.seginfo.model;
 import java.util.Collection;
 
 import ar.edu.ub.seginfo.exception.BidirectionalCipherException;
+import ar.edu.ub.seginfo.exception.BlockChainBlockException;
 import ar.edu.ub.seginfo.exception.RepositoryException;
 
 public interface IBlockChain<T> {
-	void getAll(Collection<T> collection) throws RepositoryException, BidirectionalCipherException;
+	void getAll(Collection<T> collection) throws RepositoryException, BidirectionalCipherException, BlockChainBlockException;
 }
